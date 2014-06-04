32d2b28b529ab662cdf07039f898de97ffda2e29
Craig Barretto <cbbarret@andrew.cmu.edu>
2014-06-04 07:21:35 -0400

    Added basic migrate functionality to process manager

--- src/ProcessManager.java
+++ src/ProcessManager.java
@@ -4,6 +4,8 @@
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 //import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
@@ -21,12 +23,49 @@ public class ProcessManager {
 		return currentpid.getAndIncrement();
 	}
 	
+	private MigratableProcess processByPid(long pid){
+		foreach(MigratableProcess process in processes){
+			//HOW TO ITERATE THROUGH QUEUE
+		}
+	}
+	
 	public void addProcess(String className, String[] args) throws Exception {
 		Class<?> obj = Class.forName(className);
 		Constructor contr = obj.getConstructor(Arrays.class);
 		MigratableProcess newProcess = (MigratableProcess) contr.newInstance(args);
 		newProcess.setID(currentpid.get());
 		nextPid();
+		Thread process = new Thread(newProcess);
+		process.start();
 		processes.add(newProcess);
+	}
+	
+	public void migrateProcess(String[] args){
+		long pid = Long.parseLong(args[1]);
+		String host = args[2];
+		MigratableProcess process = processByPid(pid);
+		
+		Socket socket = null;
+		socket = new Socket(host, PORT);
+		process.suspend();
+		
+		ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+		ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+		out.writeObject(process);
+		
+		if (in.readBoolean()){
+			System.out.println("Successful migration");
+			process.unsuspend();
+			process.afterMigrate();
+		}
+		else{
+			System.out.println("Failed migration");
+			//process.afterMigrate();
+		}
+			
+		
+		in.close();
+		out.close();
+		socket.close();
 	}
 }
